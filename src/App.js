faba99dedee625aacc245716209be244723935e5
Lucas Silvestre <lukas.silvestre@gmail.com>
2020-09-19 15:29:52 +0200

    feat: create keypairs and sign request to the API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import * as bip32 from 'bip32';
+import * as bip39 from 'bip39';
+import * as ecc from 'tiny-secp256k1';
 import { Header } from './components/Header'
 import { Users } from './components/Users'
 import { DisplayBoard } from './components/DisplayBoard'
@@ -17,7 +20,27 @@ class App extends Component {
   createUser = async (e) => {
     let user = this.state.user
 
-    createUser({user})
+    const mnemonic = bip39.generateMnemonic()
+    const seed = await bip39.mnemonicToSeed(mnemonic)
+
+    // [TODO] replace bip32 for a generic key derivation lib
+    const root = bip32.fromSeed(seed);
+    const path = "m/49'/1'/0'/0/0";
+    const { privateKey, publicKey } = root.derivePath(path);
+
+    const payload = {
+      action: 'registration',
+      user,
+    };
+
+    const encoder = new TextEncoder();
+    const encodedPayload = encoder.encode(JSON.stringify(payload));
+    const sha256Payload = await crypto.subtle.digest('SHA-256', encodedPayload);
+    const payloadBuffer = Buffer.from(sha256Payload);
+
+    const signature = ecc.sign(payloadBuffer, privateKey);
+
+    createUser({ publicKey, signature, payload })
       .then(response => {
         console.log(response);
         this.setState({numberOfUsers: this.state.numberOfUsers + 1})
